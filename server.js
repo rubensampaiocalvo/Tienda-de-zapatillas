8c3d3979a769322f5c647d4ef44e5f85ef5b8d28
ruben <rubensampaiocalvo2005@gmail.com>
2025-12-07 21:46:35 +0100

    trabajo terminado y documento terminado

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
-// Middleware de logs
+
 app.use((req, res, next) => {
     console.log(`📦 ${new Date().toLocaleTimeString()} - ${req.method} ${req.url}`);
     next();
@@ -426,7 +426,7 @@ app.get("/:page", (req, res) => {
     }
 });
 
-// Middleware para manejar 404
+
 app.use((req, res) => {
     console.log(`❌ Ruta no encontrada: ${req.method} ${req.url}`);
     res.status(404).json({ 
